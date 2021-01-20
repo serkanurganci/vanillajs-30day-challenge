3160202b3fc43cbbca8175bfab82c74ea366ee53
serkan urganci <serkan.urganci10@gmail.com>
2021-01-20 21:08:39 +0300

    Array Cardio Day 2 Published

---
+++ 07ArrayCardioDay2/app.js
@@ -0,0 +1,41 @@
+const people = [
+    {name: 'Wes', year:1988},
+    {name: 'Kait', year:1986},
+    {name: 'Irv', year:1970},
+    {name: 'Lux', year:2015}
+]
+
+const comments = [
+    {text: 'Love this' , id:523423},
+    {text: 'Super good' , id:823423},
+    {text: 'You are the best' , id:2039842},
+    {text: 'Ramen is my fav food ever' , id:123523},
+    {text: 'Nice Nice Nice!' , id:542328}
+];
+
+// Some and Every Checks
+    // Array.prototype.some() // is at least one person 19 or older?
+    const adultSome = people.some(person => new Date().getFullYear() - person.year >= 19);
+    console.log(adultSome)
+    // Array.prototype.every() // is everyone 19 or older?
+    const adultEvery = people.every(person => new Date().getFullYear() - person.year >= 19);
+    console.log(adultEvery)
+
+    // Array.prototype.find()
+    // Find is like filter, but instead returns just the one you are looking for
+    // find the comment with the ID of 823423
+    const findId = comments.find(comment => comment.id === 823423);
+    console.log(findId)
+
+    // Array.prototype.findIndex()
+    // Find the comment with this ID
+    // delete the comment with the ID of 823423
+
+    const findIdIndex = comments.findIndex(comment=> comment.id === 823423);
+   
+    // comments.splice(findIdIndex,1)
+
+    const newComments = [
+        ...comments.slice(0,findIdIndex),
+        ...comments.slice(findIdIndex + 1)
+    ]
