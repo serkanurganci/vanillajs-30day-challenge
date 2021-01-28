eeb1e3c9b89ae30c4fad9fcc0b572564a3ae6a12
serkanurganci <serkan.urganci.10@gmail.com>
2021-01-28 23:08:41 +0300

    Published Mouse Move Shadow Update README.md

---
+++ 16MouseMoveShadow/app.js
@@ -0,0 +1,25 @@
+// Selected Elements
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+
+function shadow(e) {
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
+  const walk = 500; //500px
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+  console.log(x,e.target.offsetLeft)
+  const xWalk = Math.round((x / width) * walk - walk / 2);
+  const yWalk = Math.round((y / height) * walk - walk / 2);
+
+  text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+    ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+    ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+    ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+    `;
+}
+
+hero.addEventListener("mousemove", shadow);
